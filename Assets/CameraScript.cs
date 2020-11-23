6857705b39b9aecdd85507795e5a7fd81a4e5e64
AutomaticComputer <nbtk.dev@gmail.com>
2020-11-23 12:56:17 +0900

    Version 0.3.4
    Added navigation buttons for devices without a keyboard.

--- Assets/CameraScript.cs
+++ Assets/CameraScript.cs
@@ -7,8 +7,16 @@ public class CameraScript : MonoBehaviour
 {
     // Start is called before the first frame update
 
+    private bool upPressed, downPressed, rightPressed, leftPressed, 
+      pageUpPressed, pageDownPressed;
     void Start()
     {
+        upPressed = false;
+        downPressed = false;
+        rightPressed = false;
+        leftPressed = false;
+        pageUpPressed = false;
+        pageDownPressed = false;
     }
 
     // Update is called once per frame
@@ -18,19 +26,69 @@ public class CameraScript : MonoBehaviour
         float verticalInput = 0.0f; 
         float depthInput = 0.0f;
 
-        if (Input.GetKey(KeyCode.UpArrow))
+        if (Input.GetKey(KeyCode.UpArrow) || upPressed)
             verticalInput = 1.0f;
-        if (Input.GetKey(KeyCode.DownArrow))
+        if (Input.GetKey(KeyCode.DownArrow) || downPressed)
             verticalInput = -1.0f;
-        if (Input.GetKey(KeyCode.RightArrow))
+        if (Input.GetKey(KeyCode.RightArrow) || rightPressed)
             horizontalInput = 1.0f;
-        if (Input.GetKey(KeyCode.LeftArrow))
+        if (Input.GetKey(KeyCode.LeftArrow) || leftPressed)
             horizontalInput = -1.0f;
-        if (Input.GetKey(KeyCode.PageUp))
+        if (Input.GetKey(KeyCode.PageUp) || pageUpPressed)
             depthInput = 1.0f;
-        if (Input.GetKey(KeyCode.PageDown))
+        if (Input.GetKey(KeyCode.PageDown) || pageDownPressed)
             depthInput = -1.0f;
 
         transform.Translate(new Vector3(horizontalInput, verticalInput, depthInput) * Time.deltaTime * 0.2f);
     }
+
+    public void UpButtonDown() 
+    {
+        upPressed = true;
+    }
+    public void UpButtonUp() 
+    {
+        upPressed = false;
+    }
+    public void DownButtonDown() 
+    {
+        downPressed = true;
+    }
+    public void DownButtonUp() 
+    {
+        downPressed = false;
+    }
+    public void RightButtonDown() 
+    {
+        rightPressed = true;
+    }
+    public void RightButtonUp() 
+    {
+        rightPressed = false;
+    }
+    public void LeftButtonDown() 
+    {
+        leftPressed = true;
+    }
+    public void LeftButtonUp() 
+    {
+        leftPressed = false;
+    }
+    public void PageUpButtonDown() 
+    {
+        pageUpPressed = true;
+    }
+    public void PageUpButtonUp() 
+    {
+        pageUpPressed = false;
+    }
+    public void PageDownButtonDown() 
+    {
+        pageDownPressed = true;
+    }
+    public void PageDownButtonUp() 
+    {
+        pageDownPressed = false;
+    }
+
 }
